ce80c61b786c4b3e7de35f15fcbbe6e9d229c2b1
Nandish Asangi <nandish.asangi@gmail.com>
2024-09-09 12:08:10 +0530

    Variables added and display message added

--- example.c
+++ example.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
+#include<conio.h>
 
 main(){
+    int x,y,z,sum;
     clrscr();
+    printf("PeopleTree College");
     printf("PeopleTree College");
     getch();
     return 0;
